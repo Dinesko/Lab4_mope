f7976bc890ff308d019df7c0c0bba3c58f328941
dinesko <borozenetsdenys@gmail.com>
2020-04-08 20:17:20 +0300

    last

--- main.py
+++ main.py
@@ -1,200 +1,156 @@
-import random
 import numpy as np
-import sklearn.linear_model as lm
+from itertools import product, combinations
+from _pydecimal import Decimal
 from scipy.stats import f, t
-from functools import partial
 
 
-def regression(x, b):
-    y = sum([x[i]*b[i] for i in range(len(x))])
-    return y
+np.set_printoptions(formatter={'float_kind': lambda x: "%.2f" % x})
 
-x_range = ((-20, 30), (-25, 10), (-25, -20))
+gt = {1: 0.6798, 2: 0.5157, 3: 0.4377, 4: 0.3910, 5: 0.3595, 6: 0.3362, 7:
+    0.3185, 8: 0.3043, 9: 0.2926, 10: 0.2829}
 
-x_aver_max = sum([x[1] for x in x_range]) / 3
-x_aver_min = sum([x[0] for x in x_range]) / 3
-
-y_max = 200 + int(x_aver_max)
-y_min = 200 + int(x_aver_min)
+tt = {16: 2.120, 24: 2.064, 32: 1.96}  # m = [3, 6]
 
 
-def s_kv(y, y_aver, n, m):
-    res = []
-    for i in range(n):
-        s = sum([(y_aver[i] - y[i][j])**2 for j in range(m)]) / m
-        res.append(round(s, 3))
-    return res
+ft = {1: {16: 4.5, 24: 4.3, 32: 4.2},
+      2: {16: 3.6, 24: 3.4, 32: 3.3},
+      3: {16: 3.2, 24: 3.0, 32: 2.9},
+      4: {16: 3.0, 24: 2.8, 32: 2.7},
+      5: {16: 2.9, 24: 2.6, 32: 2.5},
+      6: {16: 2.7, 24: 2.5, 32: 2.4}}
 
-def plan_matrix(n, m):
-    print(f'\nГереруємо матрицю планування для n = {n}, m = {m}')
-    y = np.zeros(shape=(n,m), dtype=np.int64)
-    for i in range(n):
-        for j in range(m):
-            y[i][j] = random.randint(y_min, y_max)
+def get_Gt(size_of_selections, qty_of_selections, significance ):
+    size_of_selections += 1
+    partResult1 = significance/(size_of_selections-1)
+    params = [partResult1, qty_of_selections, (size_of_selections-1-1)*qty_of_selections]
+    fisher = f.isf(*params)
+    result = fisher/(fisher+(size_of_selections-1-1))
+    return Decimal(result).quantize(Decimal('.0001')).__float__()
 
-    x_norm = [[1, -1, -1, -1],
-              [1, -1, 1, 1],
-              [1, 1, -1, 1],
-              [1, 1, 1, -1],
-              [1, -1, -1, 1],
-              [1, -1, 1, -1],
-              [1, 1, -1, -1],
-              [1, 1, 1, 1]]
 
-    for x in x_norm:
-        x.append(x[1]*x[2])
-        x.append(x[1]*x[3])
-        x.append(x[2]*x[3])
-        x.append(x[1]*x[2]*x[3])
-    x_norm = np.array(x_norm[:len(y)])
+def get_tt(f3,significance):
+    return Decimal(abs(t.ppf(significance/2,f3))).quantize(Decimal('.0001')).__float__()
 
-    x = np.ones(shape=(len(x_norm), len(x_norm[0])), dtype=np.int64)
-    for i in range(len(x_norm)):
-        for j in range(1, 4):
-            if x_norm[i][j] == -1:
-                x[i][j] = x_range[j-1][0]
-            else:
-                x[i][j] = x_range[j-1][1]
+def get_Ft(f3, f4, significance):
+    return Decimal(abs(f.isf(significance,f4,f3))).quantize(Decimal('.0001')).__float__()
 
-    for i in range(len(x)):
-        x[i][4] = x[i][1] * x[i][2]
-        x[i][5] = x[i][1] * x[i][3]
-        x[i][6] = x[i][2] * x[i][3]
-        x[i][7] = x[i][1] * x[i][3] * x[i][2]
+def make_norm_plan_matrix(plan_matrix, matrix_of_min_and_max_x):
+    X0 = np.mean(matrix_with_min_max_x, axis=1)
+    interval_of_change = np.array([(matrix_of_min_and_max_x[i, 1] - X0[i])
+                                   for i in range(len(plan_matrix[0]))])
+    X_norm = np.array(
+        [[round((plan_matrix[i, j] - X0[j]) / interval_of_change[j], 3) for j
+          in range(len(plan_matrix[i]))]
+         for i in range(len(plan_matrix))])
+    return X_norm
 
-    print('\nX:\n', x)
-    print('\nX нормоване:\n', x_norm)
-    print('\nY:\n', y)
 
-    return x, y, x_norm
+def cochran_check(Y_matrix):
+    mean_Y = np.mean(Y_matrix, axis=1)
+    dispersion_Y = np.mean((Y_matrix.T - mean_Y) ** 2, axis=0)
+    Gp = np.max(dispersion_Y) / (np.sum(dispersion_Y))
+    return Gp < gt[m - 1]
 
-def find_coef(X, Y, norm=False):
-    skm = lm.LinearRegression(fit_intercept=False)
-    skm.fit(X, Y)
-    B = skm.coef_
+def students_t_test(norm_matrix, Y_matrix):
+    mean_Y = np.mean(Y_matrix, axis=1)
+    dispersion_Y = np.mean((Y_matrix.T - mean_Y) ** 2, axis=0)
+    mean_dispersion = np.mean(dispersion_Y)
+    sigma = np.sqrt(mean_dispersion / (N * m))
+    betta = np.mean(norm_matrix.T * mean_Y, axis=1)
+    t = np.abs(betta) / sigma
+    if (m - 1) * N > 32:
+        return np.where(t > 1.96)
+    return np.where(t > tt[(m - 1) * N])
 
-    if norm == 1:
-    	print('\nКоефіцієнти рівняння регресії з нормованими X:')
+
+def phisher_criterion(Y_matrix, d):
+    if d == N:
+        return False
+    Sad = m / (N - d) * np.mean(check1 - mean_Y)
+    mean_dispersion = np.mean(np.mean((Y_matrix.T - mean_Y) ** 2, axis=0))
+    Fp = Sad / mean_dispersion
+    if (m - 1) * N > 32:
+        if N - d > 6:
+            return Fp < ft[6][32]
+        return Fp < ft[N - d][32]
+    if N - d > 6:
+        return Fp < ft[6][(m - 1) * N]
+    return Fp < ft[N - d][(m - 1) * N]
+
+
+matrix_with_min_max_x = np.array([[-20, 30], [-25, 10], [-25, -20]])
+m = 6
+N = 8
+f1 = m - 1
+f2 = N
+f3 = f1 * f2
+f4 = N - 2 # d = 2
+q = 0.05
+norm_matrix = np.array(list(product("01", repeat=3)), dtype=np.int)
+norm_matrix[norm_matrix == 0] = -1
+norm_matrix = np.insert(norm_matrix, 0, 1, axis=1)
+plan_matrix = np.empty((8, 3))
+Gt = get_Gt(f2, f1, q)
+t_tab = get_tt(f3, q)
+Ft = get_Ft(f3, f4, q)
+
+for i in range(len(norm_matrix)):
+    for j in range(1, len(norm_matrix[i])):
+        if j == 1:
+            if norm_matrix[i, j] == -1:
+                plan_matrix[i, j - 1] = 10
+            elif norm_matrix[i, j] == 1:
+                plan_matrix[i, j - 1] = 60
+        elif j == 2:
+            if norm_matrix[i, j] == -1:
+                plan_matrix[i, j - 1] = -30
+            elif norm_matrix[i, j] == 1:
+                plan_matrix[i, j - 1] = 45
+        elif j == 3:
+            if norm_matrix[i, j] == -1:
+                plan_matrix[i, j - 1] = -30
+            elif norm_matrix[i, j] == 1:
+                plan_matrix[i, j - 1] = 45
+
+plan_matr = np.insert(plan_matrix, 0, 1, axis=1)
+Y_matrix = np.random.randint(200 + np.mean(matrix_with_min_max_x, axis=0)[0],
+                             200 + np.mean(matrix_with_min_max_x, axis=0)[1], size=(N, m))
+mean_Y = np.mean(Y_matrix, axis=1)
+combination = list(combinations(range(1, 4), 2))
+for i in combination:
+    plan_matr = np.append(plan_matr, np.reshape(plan_matr[:, i[0]] *
+                                                plan_matr[:, i[1]], (8, 1)), axis=1)
+    norm_matrix = np.append(norm_matrix, np.reshape(norm_matrix[:, i[0]] *
+                                                    norm_matrix[:, i[1]], (8, 1)), axis=1)
+plan_matr = np.append(plan_matr, np.reshape(plan_matr[:, 1] * plan_matr[:, 2]
+                                            * plan_matr[:, 3], (8, 1)), axis=1)
+norm_matrix = np.append(norm_matrix, np.reshape(norm_matrix[:, 1] * norm_matrix[:, 2] * norm_matrix[:, 3], (8, 1)), axis=1)
+
+if cochran_check(Y_matrix):
+    b_natura = np.linalg.lstsq(plan_matr, mean_Y, rcond=None)[0]
+    b_norm = np.linalg.lstsq(norm_matrix, mean_Y, rcond=None)[0]
+    check1 = np.sum(b_natura * plan_matr, axis=1)
+    check2 = np.sum(b_norm * norm_matrix, axis=1)
+    indexes = students_t_test(norm_matrix, Y_matrix)
+
+    print("\nМатриця плану експерименту: \n", plan_matr)
+    print("\nНормована матриця: \n", norm_matrix)
+    print("\nМатриця відгуків: \n", Y_matrix)
+    print("\nGt:", Gt)
+    print("tt:", t_tab)
+    print("Ft:", Ft)
+    print("Середні значення У: ", mean_Y)
+    print("Натуралізовані коефіціенти: ", b_natura)
+    print("Нормовані коефіціенти: ", b_norm)
+    print("Перевірка 1: ", check1)
+    print("Перевірка 2: ", check2)
+    print("Індекси коефіціентів, які задовольняють критерію Стьюдента: ",
+          np.array(indexes)[0])
+    print("Критерій Стьюдента: ", np.sum(b_natura[indexes] *
+                                         np.reshape(plan_matr[:,indexes], (N, np.size(indexes))), axis=1))
+    if phisher_criterion(Y_matrix, np.size(indexes)):
+        print("\nРівняння регресії адекватно оригіналу.")
     else:
-    	print('\nКоефіцієнти рівняння регресії:')
-    B = [round(i, 3) for i in B]
-    print(B)
-    return B
-
-def kriteriy_cochrana(y, y_aver, n, m):
-    f1 = m - 1
-    f2 = n
-    q = 0.05
-    S_kv = s_kv(y, y_aver, n, m)
-    Gp = max(S_kv) / sum(S_kv)
-    print('\nПеревірка за критерієм Кохрена')
-    return Gp
-
-def cohren(f1, f2, q=0.05):
-    q1 = q / f1
-    fisher_value = f.ppf(q=1 - q1, dfn=f2, dfd=(f1 - 1) * f2)
-    return fisher_value / (fisher_value + f1 - 1)
-
-def bs(x, y, y_aver, n):
-    res = [sum(1 * y for y in y_aver) / n]
-    for i in range(7):
-        b = sum(j[0] * j[1] for j in zip(x[:,i], y_aver)) / n
-        res.append(b)
-    return res
-
-def kriteriy_studenta(x, y, y_aver, n, m):
-    S_kv = s_kv(y, y_aver, n, m)
-    s_kv_aver = sum(S_kv) / n
-
-    s_Bs = (s_kv_aver / n / m) ** 0.5
-    Bs = bs(x, y, y_aver, n)
-    ts = [round(abs(B) / s_Bs, 3) for B in Bs]
-
-    return ts
-
-def kriteriy_fishera(y, y_aver, y_new, n, m, d):
-    S_ad = m / (n - d) * sum([(y_new[i] - y_aver[i])**2 for i in range(len(y))])
-    S_kv = s_kv(y, y_aver, n, m)
-    S_kv_aver = sum(S_kv) / n
-
-    return S_ad / S_kv_aver
-
-def check(X, Y, B, n, m, norm=False):
-    if norm == False:
-        print('\n\tПеревірка рівняння з натуральними значеннями факторів:')
-    else:
-        print('\n\tПеревірка рівняння з нормованими значеннями факторів:')
-    f1 = m - 1
-    f2 = n
-    f3 = f1 * f2
-    q = 0.05
-
-
-    student = partial(t.ppf, q=1-0.025)
-    t_student = student(df=f3)
-
-    G_kr = cohren(f1, f2)
-
-    y_aver = [round(sum(i) / len(i), 3) for i in Y]
-    print('\nСереднє значення y:', y_aver)
-
-    disp = s_kv(Y, y_aver, n, m)
-    print('Дисперсія y:', disp)
-
-    Gp = kriteriy_cochrana(Y, y_aver, n, m)
-    print(f'Gp = {Gp}')
-    if Gp < G_kr:
-        print(f'З ймовірністю {1-q} дисперсії однорідні.')
-    else:
-        print("Необхідно збільшити кількість дослідів")
-        m += 1
-        main(n, m)
-
-    ts = kriteriy_studenta(X[:,1:], Y, y_aver, n, m)
-    print('\nКритерій Стьюдента:\n',ts)
-    res = [t for t in ts if t > t_student]
-    final_k = [B[i] for i in range(len(ts)) if ts[i] in res]
-    print('\nКоефіцієнти {} статистично незначущі, тому ми виключаємо їх з рівняння.'.format([round(i, 3) for i in B if i not in final_k]))
-
-    y_new = []
-    for j in range(n):
-        y_new.append(regression([X[j][i] for i in range(len(ts)) if ts[i] in res], final_k))
-
-    print(f'\nЗначення "y" з коефіцієнтами {final_k}')
-    print(y_new)
-
-
-    d = len(res)
-    if d >= n:
-        print('\nF4 <= 0')
-        print('')
-        return
-    f4 = n - d
-
-    F_p = kriteriy_fishera(Y, y_aver, y_new, n, m, d)
-
-    fisher = partial(f.ppf, q=0.95)
-    f_t = fisher(dfn=f4, dfd=f3)
-    print('\nПеревірка адекватності за критерієм Фішера')
-    print('Fp =', F_p)
-    print('F_t =', f_t)
-    if F_p < f_t:
-        print('Математична модель адекватна експериментальним даним')
-    else:
-        print('Математична модель не адекватна експериментальним даним')
-
-
-def main(n, m):
-
-    X, Y, X_norm = plan_matrix(n,m)
-
-    y_aver = [round(sum(i) / len(i), 3) for i in Y]
-
-    B_norm = find_coef(X_norm, y_aver, norm=True)
-
-    check(X_norm, Y, B_norm, n, m, norm=True)
-
-
-
-if __name__ == '__main__':
-	main(6, 3)
+        print("\nРівняння регресії неадекватно оригіналу.")
+else:
+    print("\n\nДисперсія неоднорідна!")
